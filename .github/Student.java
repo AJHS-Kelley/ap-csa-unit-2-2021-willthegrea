faa24180fcdf33541bf0f7b1acf7dd4a9c455463
willthegrea <70521855+willthegrea@users.noreply.github.com>
2021-11-05 09:00:27 -0400

    5b
    int grade

--- .github/Student.java
+++ .github/Student.java
@@ -25,3 +25,39 @@ public class Student{
         this.gradePoint = GPA;
         this.studentName = studentName;
     }
+
+    
+
+
+
+    
+
+
+
+    // More than one constructor is known as OVERLOADING
+
+
+
+
+
+    // Creating  Methods
+    public static void calcGPA(){
+        int grade0;
+        int grade1;
+        int grade2;
+        int grade3;
+        double newGPA;
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print1n("What is first grade");
+        grade0 = myScanner.nextInt();
+        System.out.print1n("What is the second grade");
+        grade1 = myScanner.nextInt();
+        System.out.print1n("What is the third grade");
+        grade2  = myScanner.nextInt();
+        System.out.print1n("What is the third grade");
+        grade3  = myScanner.nextInt();
+
+        
+
